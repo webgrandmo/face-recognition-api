0ecbfa8934f05c9c88f30a0e2aa61d9c8abc3c7e
Andriy Trofimov <webgrandmo@gmail.com>
2019-09-06 12:35:26 +0300

    feature/Create-endpoints. Remove commented code

--- server.js
+++ server.js
@@ -95,11 +95,3 @@ app.put('/image', (req, res) => {
 app.listen(3001, () => {
     console.log('Everything is working fine');
 });
-
-
-/*
-signin - POST  => success/fail
-register - POST => user
-profile/:id GET => user(info)
-image - PUT => image count 
-*/
